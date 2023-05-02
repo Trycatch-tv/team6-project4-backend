92129611e605783608cc14de3432869673005f0a
DiegoAAJ <diegoaraldana@hotmail.com>
2023-05-02 15:28:21 -0500

    Agregar id en usuario para el frontend

--- autenticar_api/serializers.py
+++ autenticar_api/serializers.py
@@ -12,7 +12,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(write_only=True, required=True)
     class Meta:
         model = User
-        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name')
+        fields = ('id','username', 'password', 'password2', 'email', 'first_name', 'last_name')
         extra_kwargs = {
             'first_name': {'required': True},
             'last_name': {'required': True}
